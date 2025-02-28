0cae1e81e5db132382a78ba360d6496ea7a0efa2
wyzhf <582421884@qq.com>
2025-02-28 15:54:31 +0800

    first commit

---
+++ 70.climbing-stairs.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=cpp
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+    }
+};
+// @lc code=end
+
