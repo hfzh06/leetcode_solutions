1032f3aba1dfc43a0b0b7a107535635335b282b4
hfzh <582421884@qq.com>
2025-03-11 22:34:00 +0800

    fast and slow ptr Pay Attetion to slow tinly after fast

---
+++ 83.remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=cpp
+ *
+ * [83] Remove Duplicates from Sorted List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* pre_head = new ListNode(0);
+        pre_head->next = head;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            if(fast->val == fast->next->val){
+                int x = fast->val;
+                while(fast && fast->val == x){
+                    fast = fast->next;
+                }
+                slow->next = fast;
+            }
+            else{
+                fast = fast->next;
+                slow = fast;
+            }
+        } 
+        return pre_head->next;
+    }
+};
+// @lc code=end
+
